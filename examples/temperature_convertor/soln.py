73eba7c157451e6963d08263dda5f1edf5d0679e
RohitMidha23 <rohit.midha23@gmail.com>
2020-04-20 15:08:18 +0530

    string concat and ifmain

--- examples/temperature_convertor/soln.py
+++ examples/temperature_convertor/soln.py
@@ -5,7 +5,7 @@ def fahrenheit_to_celsius():
     # use the formula to calculate temperature in celsius
     celsius = (fahrenheit - 32) * 5.0/9.0
     # output with appropriate units
-    print("Temperature:", fahrenheit, "F = ", celsius, "C")
+    print("Temperature:" + str(fahrenheit) + "F = " + str(celsius) + "C")
 
 def celsius_to_fahrenheit():
     # get the celsius temperature from the user
@@ -13,17 +13,20 @@ def celsius_to_fahrenheit():
     # use the formula to calculate temperature in fahrenheit
     fahrenheit = (celsius * 9.0/5.0) + 32
     # output with appropriate units
-    print("Temperature:", celsius, "C = ", fahrenheit, "F")
+    print("Temperature:" + str(celsius) +  "C = " + str(fahrenheit) + "F")
 
+def main():
+    # get the choice from the user
+    option = int(input("Enter choice (1/2): \n1. Fahrenheit to Celsius \n2. Celsius to Fahrenheit \n"))
 
-# get the choice from the user
-option = int(input("Enter choice (1/2): \n1. Fahrenheit to Celsius \n2. Celsius to Fahrenheit \n"))
+    # based on the option call the appropriate function
+    # if the user enters an invalid option print the appropriate response
+    if option==1:
+        fahrenheit_to_celsius()
+    elif option==2:
+        celsius_to_fahrenheit()
+    else:
+        print("INVALID OPTION!")
 
-# based on the option call the appropriate function
-# if the user enters an invalid option print the appropriate response
-if option==1:
-    fahrenheit_to_celsius()
-elif option==2:
-    celsius_to_fahrenheit()
-else:
-    print("INVALID OPTION!")
+if __name__ == '__main__':
+    main()
