e9f2253f2566dcd68f2250c7bb6296847fef0da2
atunanggara <atun.anggara@gmail.com>
2020-04-20 09:33:08 -0500

    add how-much-TP worked example

---
+++ examples/how-much-TP/soln.py
@@ -0,0 +1,24 @@
+SHEETS = 160
+USAGE = 6
+
+
+def main():
+	while True:
+		# Read the amount of Toilet Paper rolls from the user. type is float
+		number_of_rolls = float(input("Enter the amount of rolls you have: "))
+		toilet_visits = float(input("Enter the number of toilet visits per day: "))
+
+		# Calculate how long will the user's supply last
+		# How long = SHEETS * number of rolls mass / (USAGE * toilet visit)
+		how_long = int(SHEETS * number_of_rolls / (USAGE * toilet_visits))
+		
+		# Display work to the user
+		print("How long = 160 sheets per roll * number of rolls / (6 sheets per toilet visit * toilet visit per day)...")
+		print("Number of rolls = " + str(number_of_rolls) + " rolls")
+		print("Toilet visit = " + str(toilet_visits) + " per day")
+		print("You will last " + str(how_long) + " days!")
+		print("")
+
+
+if __name__ == '__main__':
+	main()
