f07407db92deb29f919f8157caa48cd10fb63968
Marek Szeles <marek.szeles@gmail.com>
2020-04-22 14:39:58 +0200

    Pi approximation through three infinite sequence methods

--- examples/pi-approximation-infinite-sequence/approximate_pi.py
+++ examples/pi-approximation-infinite-sequence/approximate_pi.py
@@ -10,26 +10,17 @@ def main():
 
         # Display different calculations to the user
         print("The Pi estimation is...")
-        print("According to Leibniz: ", leibniz_pi(iterations))
         print("According to Nilakantha: ", nilakantha_pi(iterations))
-        print("According to Newton: ", newton_pi(iterations))
+        print("According to Wallis: ", wallis_pi(iterations))
+        print("According to Leibniz: ", leibniz_pi(iterations))
         print("")
-
-
-def leibniz_pi(x):
-    # Leibniz used this approximation in 1674:
-    # 4 / 1 - 4 / 3 + 4 / 5 - 4 / 7 + 4 / 9 - ...
-    p = 0.0
-    for i in range(1, x + 1):
-        p = p + ((-1 * math.pow(-1, i)) * 4 / (2 * i - 1))
-    return p
 
 
 def nilakantha_pi(x):
     # An infinite series for PI published by Nilakantha in the 15th century goes as follows:
     # 3 + (4 / 2 * 3 * 4) - (4 / 4 * 5 * 6) + (4 / 6 * 7 * 8) - ...
     p = 3.0
-    for i in range(1, x + 1):
+    for i in range(2, x + 1):
         if i == 1:
             p = 3.0
         else:
@@ -37,15 +28,22 @@ def nilakantha_pi(x):
     return p
 
 
-def newton_pi(x):
-    # One of the simpler methods Newton used was to refine the ratio between sine and cosine in a circle
-    old = 3.0
-    new = old - (math.sin(old)) / (math.cos(old))
-    while x != 0:
-        old = new
-        new = old - (math.sin(old)) / (math.cos(old))
-        x -= 1
-    return new
+def wallis_pi(x):
+    # This method was found by John Wallis in 1655:
+    # (2 / 1) * (2 / 3) * (4 / 3) * (4 / 5) * (6 / 5) * ...
+    p = 2.0
+    for i in range(1, x):
+        p = p * ((2 * i) / (1 + 2 * i)) * ((2 * (i + 1)) / (1 + 2 * i))
+    return p * 2
+
+
+def leibniz_pi(x):
+    # Leibniz used this approximation in 1674:
+    # 4 / 1 - 4 / 3 + 4 / 5 - 4 / 7 + 4 / 9 - ...
+    p = 4.0
+    for i in range(2, x + 1):
+        p = p + ((-1 * math.pow(-1, i)) * 4 / (2 * i - 1))
+    return p
 
 
 if __name__ == '__main__':
