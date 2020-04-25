71d21f30ea74d37845215b8c5dd92356389aa720
Mbaoma Chioma (Omarrr) <49791498+Mbaoma@users.noreply.github.com>
2020-04-25 02:37:51 +0100

    Update soln.py
    Edited the program for better readability

--- examples/subtract_numbers/soln.py
+++ examples/subtract_numbers/soln.py
@@ -4,12 +4,15 @@
 
 #define the function
 def main():
+    #print the title of the program
+    print('This program subtracts one number from another.')
+
     #accept the first input
     #float keyword converts the input to a decimal number for uniformity
-    first_number = float(input('Kindly enter a number: '))
+    first_number = float(input('Enter first number: '))
 
     #accept the second input
-    second_number = float(input('Kindly enter another number: '))
+    second_number = float(input('Enter second number: '))
 
     #subtract the second input from the first input
     result = first_number - second_number
