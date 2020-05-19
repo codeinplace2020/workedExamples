4bac107ca29b82af4d7dcd65b4ee04c8d0cfe32e
Ruko97 <ruko97@gmail.com>
2020-05-19 20:18:25 +0600

    Changed soln.py - added a while loop to the code

--- examples/am_i_18/soln.py
+++ examples/am_i_18/soln.py
@@ -1,13 +1,14 @@
 AGE_LIMIT = 18
 
 def main():
-    age_str = input("Enter your age: ")
-    age = int(age_str)
-    if age >= AGE_LIMIT:
-        print("You're old enough to vote!")
-    else:
-        print("Sorry, you can't vote yet!")
-    print('')
+    while True:
+        age_str = input("Enter your age: ")
+        age = int(age_str)
+        if age >= AGE_LIMIT:
+            print("You're old enough to vote!")
+        else:
+            print("Sorry, you can't vote yet!")
+        print('')
 
 if __name__ == '__main__':
     main()
